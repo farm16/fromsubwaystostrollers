09f5e3c232e1887ad33858c6e833c08ac33dd851
christopher73 <christopher.fajardo73@gmail.com>
2020-01-16 00:28:00 -0500

    navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,55 +1,55 @@
-import React, { useState } from 'react';
-import '../scss/main.scss';
+import React, { useState } from "react";
+import "../scss/main.scss";
 
 export default function NavBar() {
   const [toggle, setToggle] = useState({ istoggle: false });
 
   function toggleMenu() {
-    const hamburger = document.querySelector('.menu-btn__burger');
-    const nav = document.querySelector('.nav');
-    const menuNav = document.querySelector('.menu-nav');
-    const navItems = document.querySelectorAll('.menu-nav__item');
+    const hamburger = document.querySelector(".menu-btn__burger");
+    const nav = document.querySelector(".nav");
+    const menuNav = document.querySelector(".menu-nav");
+    const navItems = document.querySelectorAll(".menu-nav__item");
 
     if (!toggle.istoggle) {
-      hamburger.classList.add('open');
-      nav.classList.add('open');
-      menuNav.classList.add('open');
-      navItems.forEach(item => item.classList.add('open'));
+      hamburger.classList.add("open");
+      nav.classList.add("open");
+      menuNav.classList.add("open");
+      navItems.forEach(item => item.classList.add("open"));
       setToggle({ istoggle: true });
     } else {
-      hamburger.classList.remove('open');
-      nav.classList.remove('open');
-      menuNav.classList.remove('open');
-      navItems.forEach(item => item.classList.remove('open'));
+      hamburger.classList.remove("open");
+      nav.classList.remove("open");
+      menuNav.classList.remove("open");
+      navItems.forEach(item => item.classList.remove("open"));
       setToggle({ istoggle: false });
     }
   }
   return (
     <header>
-      <div className='menu-btn' onClick={toggleMenu}>
-        <span className='menu-btn__burger'></span>
+      <div className="menu-btn" onClick={toggleMenu}>
+        <span className="menu-btn__burger"></span>
       </div>
-
-      <nav className='nav'>
-        <ul className='menu-nav'>
-          <li className='menu-nav__item active'>
-            <a href='index.html' className='menu-nav__link'>
-              Home
+      <nav className="nav">
+        <h1>From Subways to Strollers</h1>
+        <ul className="menu-nav">
+          <li className="menu-nav__item active">
+            <a href="index.html" className="menu-nav__link">
+              Motherhood
             </a>
           </li>
-          <li className='menu-nav__item'>
-            <a href='about.html' className='menu-nav__link'>
-              About Me
+          <li className="menu-nav__item">
+            <a href="about.html" className="menu-nav__link">
+              Natural Remedies
             </a>
           </li>
-          <li className='menu-nav__item'>
-            <a href='projects.html' className='menu-nav__link'>
-              My Projects
+          <li className="menu-nav__item">
+            <a href="projects.html" className="menu-nav__link">
+              Food
             </a>
           </li>
-          <li className='menu-nav__item'>
-            <a href='contact.html' className='menu-nav__link'>
-              Contact Me
+          <li className="menu-nav__item">
+            <a href="contact.html" className="menu-nav__link">
+              NYC
             </a>
           </li>
         </ul>
